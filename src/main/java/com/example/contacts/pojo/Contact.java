4096ce10c8650ae18f7c613600d55124d3dfa2cf
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-08-15 06:50:12 -0600

    refactor (contact) added constructors

--- src/main/java/com/example/contacts/pojo/Contact.java
+++ src/main/java/com/example/contacts/pojo/Contact.java
@@ -1,13 +1,29 @@
 package com.example.contacts.pojo;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
 public class Contact {
 
     private String id;
+
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Phone number cannot be blank")
     private String phoneNumber;
 
+    public Contact(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 
     public Contact(){
         this.id = UUID.randomUUID().toString();
